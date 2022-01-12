516966a820f08b26625e77afca8c9b86f8738159
strakins <akinolablessing25@gmail.com>
2022-01-12 10:21:18 +0100

    Footer Content Improved

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react"
 
 const Footer = () => (
     <Box textAlign="center" p="6" bg="gray.400" color="white" borderTop="2px" borderColor="black">
-        2022 <a href="https://www.linkedin.com/in/blessing-akinola-0073b9112/" passHref>Strakins</a> , Inc. 
+       Developed By <a href="https://www.linkedin.com/in/blessing-akinola-0073b9112/" passHref>Strakins</a>,  2022 Inc. 
     </Box>
 )
 
